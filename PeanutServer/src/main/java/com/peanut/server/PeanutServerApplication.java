5b1984c331529e46d3bc7ed281e9ebccd787cf09
Romain <romain.dalichamp@free.fr>
2019-08-30 15:03:31 -0400

    First Commit

---
+++ PeanutServer/src/main/java/com/peanut/server/PeanutServerApplication.java
@@ -0,0 +1,13 @@
+package com.peanut.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeanutServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeanutServerApplication.class, args);
+	}
+
+}
