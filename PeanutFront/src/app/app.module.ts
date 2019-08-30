f8d8246d19af19c74e271eb067b10932645484c4
Romain <romain.dalichamp@free.fr>
2019-08-30 15:56:33 -0400

    Install Jquery and Bootstrap in Front Side + main Components Products-Register and Login

--- PeanutFront/src/app/app.module.ts
+++ PeanutFront/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
